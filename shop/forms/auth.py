5907d6d1b75f1baa357305f7a49f75641440b45c
Manish Mahesh Kumar <manish.manish.mahesh.kumar@tuhh.de>
2024-04-21 20:48:48 +0200

    Phase 1 Complete

--- shop/forms/auth.py
+++ shop/forms/auth.py
@@ -67,7 +67,7 @@ class RegisterUserForm(NgModelFormMixin, NgFormValidationMixin, UniqueEmailValid
     def clean(self):
         cleaned_data = super().clean()
         password1 = cleaned_data.get('password1')
-        password2 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
         if password1 and password2:
             if password1 != password2:
                 raise ValidationError(
